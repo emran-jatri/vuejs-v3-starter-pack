bea5e64606a43462b6c40242f9580127fa173055
emran-jatri <emran.jatri@gmail.com>
2023-01-15 17:19:30 +0600

    tailwind added

---
+++ src/views/header/HeaderOne.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="bg-blue-500 p-5">
+    <div class="flex justify-between items-center">
+      <RouterLink to="/" class="text-white text-2xl font-bold uppercase">Logo</RouterLink>
+      <div class="flex space-x-4">
+        <RouterLink to="/" class="text-white" >Services</RouterLink>
+        <RouterLink to="/" class="text-white" >Products</RouterLink>
+        <RouterLink to="/about" class="text-white" >About</RouterLink>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/views/pages/HomePage.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref, reactive, onMounted } from "vue";
+
+onMounted(() => {
+  console.log(`the component is now mounted.`);
+});
+
+const countOne = ref(0);
+// console.log("🚀 ~ file: HomePage.vue:5 ~ countOne", countOne.value);
+const counter = reactive({ countTwo: 0 });
+// console.log("🚀 ~ file: HomePage.vue:7 ~ counter", counter);
+</script>
+
+<template>
+  <div>
+    <p class="text-center text-2xl">Home Page</p>
+    <div>
+      <p>Count One: {{ countOne }}</p>
+      <p>Count One: {{ counter.countTwo }}</p>
+    </div>
+    <div class="space-x-2 mt-5">
+      <button class="bg-blue-500 p-2 text-white" @click="countOne++">
+        CountOne
+      </button>
+      <button class="bg-blue-500 p-2 text-white" @click="counter.countTwo++">
+        CountTwo
+      </button>
+    </div>
+  </div>
+</template>
